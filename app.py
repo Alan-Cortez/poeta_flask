0470fbaa6c4f96ca94e8fc46580867f2014254fe
Alan-Cortez <149338618+Alan-Cortez@users.noreply.github.com>
2024-09-09 17:27:32 -0600

    Update and rename hello.py to app.py

--- app.py
+++ app.py
@@ -15,4 +15,6 @@ def alumnos():
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-    return f"Matrícula: {request.form["txtMatriculaFA"]} Nombre y Apellido: {request.form["txtNombreApellidoFA"]}"
+    matricula:    =request.form["txtMatricula"]
+    nombreapellido=request.form["txtNombreApellido"]
+    return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
