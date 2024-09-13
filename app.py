ff4c20597215f4ff517281a9d3f3c8d22e472131
Alan-Cortez <149338618+Alan-Cortez@users.noreply.github.com>
2024-09-12 19:28:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,10 @@ def alumnosGuardar():
 @app.route("/evento, methods=["GET"]")
 def evento():
     pusher_client = pusher.Pusher(
-        app_id="1714541",
-        key="cda1cc599395d699a2af",
-        secret="9e9c00fc36600060d9e2",
-        cluster="us2",
+        app_id='1767934',
+        key='ffa9ea426828188c22c1',
+        secret='628348e447718a9eec1f',
+        cluster='us2',
         ssl=True
     )
     
